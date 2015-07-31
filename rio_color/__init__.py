741e6e1d00b794ec924a10199cbb0ddee8ba0512
Charlie Loyd <charlie@mapbox.com>
2015-07-30 17:22:37 -0700

    initial color operations

--- rio_color/__init__.py
+++ rio_color/__init__.py
@@ -1,2 +1,89 @@
-# rio_color
+import rasterio as rio
+import numpy as np
+import sys
+from math import exp, ceil
+
 __version__ = '0.0.0'
+
+# The type to be used for all intermediate math
+# operations. Should be a float because values will
+# be scaled to the range 0..1 for all work.
+math_type = np.float32
+
+epsilon = np.finfo(math_type).eps
+
+# The largest value representable by uint16:
+max_int = np.iinfo(np.uint16).max
+
+
+def to_math_type(arr):
+  '''Convert an array from uint16 to 0..1, scaling down linearly'''
+  return arr.astype(math_type)/max_int
+
+def to_uint16(arr):
+  '''Convert an array from 0..1 to uint16, scaling up linearly'''
+  return (arr*max_int).astype(np.uint16)
+
+
+# Color manipulation functions
+
+def sigmoidal(arr, bias, contrast):
+
+  alpha, beta = bias, contrast
+  # We use the names a and b to match documentation.
+  
+  if alpha == 0:
+    alpha = epsilon
+  
+  if beta == 0:
+    return arr
+  
+  np.seterr(divide='ignore', invalid='ignore')
+  
+  if beta > 0:
+    # Forward sigmoidal function:
+    # (This is really badly documented in the wild/internet.
+    # @virginiayung is the only person I trust to understand it.)
+    numerator   = 1/(1+np.exp(beta*(alpha-arr))) - 1/(1+np.exp(beta*alpha))
+    denominator = 1/(1+np.exp(beta*(alpha - 1))) - 1/(1+np.exp(beta*alpha))
+    return numerator / denominator
+  else:
+    # Inverse sigmoidal function:
+    # todo: account for 0s
+    # todo: formatting ;)
+    return (
+      (beta*alpha) - np.log(
+        (
+          1 / (
+            (arr/(1 + np.exp( beta*alpha - beta )))
+            - (arr / (1 + np.exp( beta*alpha )))
+            + (1 / (1 + np.exp( beta*alpha )))
+          )
+        )
+        - 1)
+      ) / beta
+
+
+
+def gamma(arr, g):
+  return arr**(1.0/g)
+
+
+# Utility functions
+
+def simple_atmo(rgb, haze, contrast, bias):
+  '''A simple, non-adaptive atmospheric correction function.'''
+	
+  rgb = to_math_type(rgb)
+	
+  bias /= 100.0 # easier to express in the range 0..100
+  
+  gamma_b = 1 - haze
+  gamma_g = 1 - (haze*(1/3.0))
+
+  rgb[1] = gamma(rgb[1], gamma_g)  
+  rgb[2] = gamma(rgb[2], gamma_b)
+
+  return to_uint16(
+    sigmoidal(rgb, midpoint, contrast)
+  )
--- rio_color/scripts/cli.py
+++ rio_color/scripts/cli.py
@@ -1,4 +1,57 @@
-@click.version_option(version=metasay_version, message='%(version)s')
+import click
+import rasterio as rio
+import rio_color
+
+@click.command('color')
+@click.option(
+  '--atmo',
+  '-a',
+  type=click.FLOAT,
+  default=0.03,
+  help='How much to dampen cool colors, thus cutting through haze. 0..1, default 0.03.')
+
+@click.option(
+  '--contrast',
+  '-c',
+  type=click.FLOAT,
+  default=10,
+  help='Contrast factor to apply to the scene. -infinity..infinity (0 is none), default 10.'
+)
+
+@click.option(
+  '--midpoint',
+  '-m',
+  type=click.FLOAT,
+  default=15,
+  help='Skew (brighten/darken) the output. Lower values mean it\'s brighter. 0..infinity, default 15.'
+)
+
+@click.argument('src_path', type=click.Path(exists=True))
+@click.argument('dst_path', type=click.Path(exists=False))
+#@click.version_option(version=metasay_version, message='%(version)s')
 @click.pass_context
-def rio_color(ctx, inputfile):
-    #here
+def rio_color(ctx, atmo, contrast, midpoint, src_path, dst_path):
+
+  # todo: test that image is 16 bits, 3 bands, etc.
+  # todo: think about mask/alpha
+  bidxs = 1, 2, 3
+
+  with rio.open(src_path) as src:
+    src_meta = src.meta
+    src_meta.update(photometric='RGB')
+    src_meta.update(transform=src_meta['affine'])
+
+    with rio.open(dst_path, 'w', **src_meta) as dst:
+    
+      inp = src.read()
+    
+      rgb_adj = quats.simple_atmo(
+        inp,
+        atmo,
+        contrast,
+        midpoint)
+		
+      for bidx in bidxs:
+        dst.write(
+          rgb_adj[bidx-1],
+          bidx)
