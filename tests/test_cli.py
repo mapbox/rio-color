fdeccd134e3af730464a19dc77fbd12e377f6f7e
Matthew Perry <perrygeo@gmail.com>
2016-04-19 15:49:18 -0400

    rm debug

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -210,6 +210,5 @@ def test_color_cli_16bit_photointerp(tmpdir):
 
     with rasterio.open('tests/rgb16.tif') as src:
         with rasterio.open(output) as out:
-            raise Exception
             for b in src.indexes:
                 assert out.colorinterp(b) == src.colorinterp(b)
