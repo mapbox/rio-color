c0373f848c2b847abbef9e994cf8861f9cc61ae2
Sean Gillies <sean@mapbox.com>
2016-05-24 13:19:32 -0600

    Parametrize color round trip tests
    I think we may have been hiding failures from ourselves by using
    non-RGB colors computed by rio-color as input for converting back
    to RGB. These new tests use independent color values.

--- tests/test_colorspace.py
+++ tests/test_colorspace.py
@@ -1,4 +1,5 @@
-import itertools
+from itertools import combinations, product
+import math
 
 import numpy as np
 import pytest
@@ -102,6 +103,7 @@ def test_bad_array_bands():
         convert_arr(bad, cs.rgb, cs.lch)
     assert '3 bands' in str(exc.value)
 
+
 def test_bad_array_dims():
     bad = np.random.random((3, 3))
     with pytest.raises(ValueError) as exc:
@@ -126,21 +128,28 @@ def test_bad_array_type():
 
 def test_array_bad_colorspace():
     arr = np.random.random((3, 3))
-    with pytest.raises(ValueError) as exc:
+    with pytest.raises(ValueError):
         convert_arr(arr, src='FOO', dst='RGB')
 
     with pytest.raises(ValueError):
         convert_arr(arr, src=999, dst=999)
 
 
-def test_bad_colorspace():
-    with pytest.raises(ValueError) as exc:
+def test_bad_colorspace_string():
+    """String colorspaces raise ValueError"""
+    with pytest.raises(ValueError):
         convert(0.1, 0.1, 0.1, src='FOO', dst='RGB')
 
+
+def test_bad_colorspace_invalid_int():
+    """Invalid colorspace integers raise ValueError"""
     with pytest.raises(ValueError):
         convert(0.1, 0.1, 0.1, src=999, dst=999)
 
-    with pytest.raises(AttributeError) as exc:
+
+def test_bad_colorspace_invalid_enum():
+    """Invalid colorspace enum names raise AttributeError"""
+    with pytest.raises(AttributeError):
         convert(0.1, 0.1, 0.1, src=cs.foo, dst=cs.bar)
 
 
@@ -149,27 +158,101 @@ def _iter_floateq(a, b, tol):
         return abs(x - y) < tol
 
 
-def test_convert_roundtrips():
+@pytest.mark.parametrize(
+    "rgb", list(combinations([0, 0.1, 0.3, 0.6, 0.9, 1.0], 3)))
+def test_convert_roundtrips(rgb):
     cspaces = tuple(x for x in dict(cs.__members__).values())
     # TODO relative tolerance and figure out how to decrease float drift
     tolerance = 0.01
+    colors = {}
 
-    for rgb in itertools.combinations([0, 0.1, 0.3, 0.6, 0.9, 1.0], 3):
-        colors = {}
+    # RGB to other, roundtrip
+    for cspace in cspaces:
+        if cspace == cs.rgb:
+            continue
+        other = convert(*rgb, src=cs.rgb, dst=cspace)
+        # Collect some valid colors
+        colors[cspace] = other
+        back = convert(*other, src=cspace, dst=cs.rgb)
+        assert _iter_floateq(back, rgb, tol=tolerance)
 
-        # RGB to other, roundtrip
-        for cspace in cspaces:
-            if cspace == cs.rgb:
-                continue
-            other = convert(*rgb, src=cs.rgb, dst=cspace)
-            # Collect some valid colors
-            colors[cspace] = other
-            back = convert(*other, src=cspace, dst=cs.rgb)
-            assert _iter_floateq(back, rgb, tol=tolerance)
+    # other to other roundtrip, including self
+    for src, color in colors.items():
+        for dst in cspaces:
+            other = convert(*color, src=src, dst=dst)
+            back = convert(*other, src=dst, dst=src)
+            assert _iter_floateq(back, color, tol=tolerance)
 
-        # other to other roundtrip, including self
-        for src, color in colors.items():
-            for dst in cspaces:
-                other = convert(*color, src=src, dst=dst)
-                back = convert(*other, src=dst, dst=src)
-                assert _iter_floateq(back, color, tol=tolerance)
+
+# Color values to use when parametrizing color conversion tests.
+# There are 4 classes of characteristic values.
+#
+# RGB, XYZ, and C values range from 0-1.
+# Note: RGB (0, 0, 0) can't be converted to LUV and so we xfail that
+# case when parametrizing (below).
+rgbxyzc_vals = [0.0, 0.3, 0.6, 1.0]
+
+# L ranges from 0-100.
+L_vals = [0.0, 50.0, 100.0]
+
+# ab and uv range from -inf to inf.
+abuv_vals = [-10.0, 0.0, 10.0]
+
+# H ranges from -pi to pi.
+h_vals = [-math.pi / 2.0, 0.0, math.pi / 2.0]
+
+# In parametrizing colors, we use `itertools.product()` with 3 of the
+# sequences above to get a grid or matrix of colors in a particular
+# colorspace.
+rgb_colors = xyz_colors = list(product(rgbxyzc_vals, repeat=3))
+lab_colors = list(product(L_vals, abuv_vals, abuv_vals))
+lch_colors = list(product(L_vals, rgbxyzc_vals, h_vals))
+luv_colors = list(product(L_vals, abuv_vals, abuv_vals))
+
+
+def assert_color_roundtrip(color, src, dst, tolerance):
+    """Asserts roundtrip of color correction within a given tolerance
+
+    Helper function for tests below.
+    """
+    other = convert(*color, src=src, dst=dst)
+    back = convert(*other, src=dst, dst=src)
+    assert _iter_floateq(back, color, tol=tolerance)
+
+
+# In parametrizing destination colorspaces we use a list comprehension,
+# omitting the source colorspace.
+
+@pytest.mark.parametrize("color", rgb_colors)
+@pytest.mark.parametrize("dst", [v for v in cs if v != cs.rgb])
+@pytest.mark.parametrize("tolerance", [0.01])
+def test_rgb_convert_roundtrip(color, dst, tolerance):
+    assert_color_roundtrip(color, cs.rgb, dst, tolerance)
+
+
+@pytest.mark.parametrize("color", xyz_colors)
+@pytest.mark.parametrize("dst", [v for v in cs if v != cs.xyz])
+@pytest.mark.parametrize("tolerance", [0.01])
+def test_xyz_convert_roundtrip(color, dst, tolerance):
+    assert_color_roundtrip(color, cs.xyz, dst, tolerance)
+
+
+@pytest.mark.parametrize("color", lab_colors)
+@pytest.mark.parametrize("dst", [v for v in cs if v != cs.lab])
+@pytest.mark.parametrize("tolerance", [0.01])
+def test_lab_convert_roundtrip(color, dst, tolerance):
+    assert_color_roundtrip(color, cs.lab, dst, tolerance)
+
+
+@pytest.mark.parametrize("color", lch_colors)
+@pytest.mark.parametrize("dst", [v for v in cs if v != cs.lch])
+@pytest.mark.parametrize("tolerance", [0.01])
+def test_lch_convert_roundtrip(color, dst, tolerance):
+    assert_color_roundtrip(color, cs.lch, dst, tolerance)
+
+
+@pytest.mark.parametrize("color", luv_colors)
+@pytest.mark.parametrize("dst", [v for v in cs if v != cs.luv])
+@pytest.mark.parametrize("tolerance", [0.01])
+def test_luv_convert_roundtrip(color, dst, tolerance):
+    assert_color_roundtrip(color, cs.luv, dst, tolerance)
