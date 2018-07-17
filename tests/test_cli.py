4b91edebb3030acfdd575b8ab22e25e79cf8f3dd
Sean C. Gillies <sean@mapbox.com>
2018-07-17 16:52:13 -0600

    Explicitly set color interpretation
    Previously we were counting on creation keyword storage that is
    gone from Rasterio 1.0.
    
    Resolves #51

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -211,8 +211,7 @@ def test_color_cli_16bit_photointerp(tmpdir):
 
     with rasterio.open('tests/rgb16.tif') as src:
         with rasterio.open(output) as out:
-            for b in src.indexes:
-                assert out.colorinterp == src.colorinterp
+            assert out.colorinterp == src.colorinterp
 
 
 def test_color_empty_operations(tmpdir):
